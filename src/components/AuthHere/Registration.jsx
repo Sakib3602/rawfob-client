5b7c0c3ea2ace3c473aded710011ecf9f5925032
Sakib3602 <sakibsarker6969@gmail.com>
2024-06-01 22:58:16 +0600

    react hook form added

--- src/components/AuthHere/Registration.jsx
+++ src/components/AuthHere/Registration.jsx
@@ -1,25 +1,35 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { CiUser } from "react-icons/ci";
+import { useForm } from "react-hook-form"
 
 const Registration = () => {
-  const  [cng, setCng] = useState(false)
-    function handleChange(){
-      setCng(!cng)
-    }
+  const [cng, setCng] = useState(false);
+  function handleChange() {
+    setCng(!cng);
+  }
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+  const onSubmit = (data) => console.log(data.email)
   return (
     <div className="w-full md:w-[80%] lg:w-[80%] m-auto md:border lg:border mt-10 p-5">
       <h1 className="text-center text-[40px] font-[700]">Register Now!</h1>
-      <form className="form m-auto">
+      <form onSubmit={handleSubmit(onSubmit)} className="form m-auto">
         {/*  */}
         <div className="flex-column">
           <label>User Name </label>
         </div>
         <div className="inputForm">
-        <CiUser className="font-[700]"></CiUser>
+          <CiUser className="font-[700]"></CiUser>
 
-          <input type="text" className="input" placeholder="Your Name" />
+          <input type="text" {...register("name", { required: true })}  className="input" placeholder="Your Name" />
         </div>
+        {errors.name && <span className="text-red-600">This field is required</span>}
         {/*  */}
         <div className="flex-column">
           <label>Email </label>
@@ -36,27 +46,20 @@ const Registration = () => {
             </g>
           </svg>
 
-
-
-
-
-
-
-          <input type="text" className="input" placeholder="Enter your Email" />
+          <input type="text" {...register("email", { required: true })}  className="input" placeholder="Enter your Email" />
         </div>
+          {errors.email && <span className="text-red-600">This field is required</span>}
         <label className="form-control w-full max-w-xs">
-  <div className="label">
-    <span className="label-text font-[600] text-[16px]">Image</span>
-    
-  </div>
-  <input type="file" className="file-input file-input-bordered w-full max-w-xs" />
-  
-</label>
-
-
-
-
-
+          <div className="label">
+            <span className="label-text font-[600] text-[16px]">Image</span>
+          </div>
+          <input
+            type="file"
+            className="file-input file-input-bordered w-full max-w-xs"
+            {...register("image", { required: true })} 
+          />
+        </label>
+          {errors.image && <span className="text-red-500">This field is required</span>}
 
         <div className="flex-column">
           <label>Password </label>
@@ -72,19 +75,13 @@ const Registration = () => {
             <path d="m304 224c-8.832031 0-16-7.167969-16-16v-80c0-52.929688-43.070312-96-96-96s-96 43.070312-96 96v80c0 8.832031-7.167969 16-16 16s-16-7.167969-16-16v-80c0-70.59375 57.40625-128 128-128s128 57.40625 128 128v80c0 8.832031-7.167969 16-16 16zm0 0" />
           </svg>
 
-
-
-
-
-
-
-
-
           <input
             type={cng ? "text" : "password"}
             className="input"
             placeholder="Enter your Password"
+            {...register("password", { required: true ,  minLength: 6 })}
           />
+          
           <svg
             viewBox="0 0 576 512"
             onClick={handleChange}
@@ -94,6 +91,8 @@ const Registration = () => {
             <path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z" />
           </svg>
         </div>
+        {errors.password && <span className="text-red-500">This field is require and give upto 6 keywords.</span>}
+       
         <div className="flex-row">
           <div>
             <input type="checkbox" />
